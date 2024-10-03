6d6950e4443d65a76b07b934cfcd18536c7c7b25
leyl1ne <e-khodin@inbox.ru>
2024-10-03 15:17:01 +0300

    add struct Userdata and add global variables; create func openConnection, exists and AddUser

---
+++ post05.go
@@ -0,0 +1,116 @@
+package post05
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	_ "github.com/lib/pq"
+)
+
+type Userdata struct {
+	ID          int
+	Username    string
+	Name        string
+	Surname     string
+	Description string
+}
+
+// все эти переменные должны быть инициализированы go кодом
+// который будет использовать этот пакет
+var (
+	Hostname = ""
+	Port     = 2345
+	Username = ""
+	Password = ""
+	Database = ""
+)
+
+// определяем закрытую функцию
+func openConnection() (*sql.DB, error) {
+	// строка подключения
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		Hostname, Port, Username, Password, Database)
+
+	// открыть базу данных
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// Функция возвращает ID пользователя username
+// -1, если пользователь не существует
+func exists(username string) int {
+	username = strings.ToLower(username)
+
+	db, err := openConnection()
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	defer db.Close()
+
+	userID := -1
+	statement := fmt.Sprintf(`SELECT "id" FROM "users" where username = '%s'`, username)
+	rows, err := db.Query(statement)
+
+	for rows.Next() {
+		var id int
+		err = rows.Scan(&id)
+		if err != nil {
+			fmt.Println("Scan", err)
+			return -1
+		}
+		userID = id
+	}
+	defer rows.Close()
+	return userID
+}
+
+// AddUser() добавляет нового пользователя в базу данных
+// Возвращаем новый ID пользоывтеля
+// -1, если произошла ошибка
+func AddUser(d Userdata) int {
+	// в нижний регистр чтобы избежать дублирования
+	d.Username = strings.ToLower(d.Username)
+
+	db, err := openConnection()
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	defer db.Close()
+
+	userID := exists(d.Username)
+	if userID != -1 {
+		fmt.Println("User already exists:", Username)
+		return -1
+	}
+
+	// в insertStatement кладем переменную d.Username
+	// для предоставленного запроса требуется одно значение $1
+	insertStatement := `insert into "users" ("username") values ($1)`
+	_, err = db.Exec(insertStatement, d.Username)
+
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+
+	userID = exists(d.Username)
+	if userID == -1 {
+		return userID
+	}
+
+	// нужны теперь 4 значения $1, $2, $3, $4
+	insertStatement = `insert into "userdata" ("userid", "name", "surname", description") values ($1, $2, $3, $4)`
+	_, err = db.Exec(insertStatement, userID, d.Name, d.Surname, d.Description)
+	if err != nil {
+		fmt.Println("db.Exec()", err)
+		return -1
+	}
+
+	return userID
+}
